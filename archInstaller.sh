06625a111dbdf750835c886a9fd20becdc9e51e0
CarlosR759 <carlos.uni.pucv@gmail.com>
2025-02-11 10:33:52 -0300

    Adding parallel downloads support

--- archInstaller.sh
+++ archInstaller.sh
@@ -93,6 +93,7 @@ echo "Done. Proceeding with Arch Linux installation."
 
 ### Needs to install amd or intel microcode
 ### INSTALL LIST AND ARCH CHROOT ###
+sed -i 's/^#ParallelDownloads = 5/ParallelDownloads = 5/' /etc/pacman.conf
 pacstrap -K /mnt base linux linux-firmware grub man-db man-pages texinfo vi vim eza networkmanager ntp bat alacritty kitty sudo fastfetch ufw
 
 genfstab -U /mnt >> /mnt/etc/fstab
