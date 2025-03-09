9785bdf17013a30b35d103477594708b907e19ae
CarlosR759 <carlos.uni.pucv@gmail.com>
2025-03-09 20:51:42 -0300

    Adding encrypt drive support

--- archInstaller.sh
+++ archInstaller.sh
@@ -37,29 +37,32 @@ echo "You will need to select a drive for your arch linux installation: for exam
 echo
 read -r -p "Please insert your desire drive to make installation: " DISK
 echo
-
+read -r -p "Do you want to encrypt root partition? (yes/no) : " encryptFlag
+echo
 echo "Creating partitions on $DISK..."
 
 ### This if make the partitions for bios or uefi depending the case.
-if [ "$BiosOrUefi" = '64' ]
+if [ "$encryptFlag" = 'no' ]
 then
-	# Create a GPT partition table (for UEFI systems)
-	sudo fdisk "$DISK" << EOF
-	g  # Create a new empty partition table (GPT)
-	n  # New partition for EFI System (512MB)
-	p
-	1
+  if [ "$BiosOrUefi" = '64' ]
+  then
+  	# Create a GPT partition table (for UEFI systems)
+  	sudo fdisk "$DISK" << EOF
+  	g  # Create a new empty partition table (GPT)
+  	n  # New partition for EFI System (512MB)
+  	p
+  	1
 
-	+512M
-	n  # New partition for root (/) (remaining space)
-	p
-	2
+  	+512M
+  	n  # New partition for root (/) (remaining space)
+  	p
+  	2
 
 
-	w  # Write changes and exit
-EOF
-elif [ "$BiosOrUefi" = '32' ]
-then
+  	w  # Write changes and exit
+  EOF
+  elif [ "$BiosOrUefi" = '32' ]
+  then
     sudo fdisk "$DISK" << EOF
     g  # Create a new empty partition table (GPT)
     n  # New partition for EFI System (512MB)
@@ -73,9 +76,9 @@ then
 
 
     w  # Write changes and exit
-EOF
-elif [ "$BiosOrUefi" = '0' ]
-then
+  EOF
+  elif [ "$BiosOrUefi" = '0' ]
+  then
     sudo fdisk  "$DISK" << EOF
     o # Create new empy partition table (MBR)
     n  # New partition for EFI System (512MB)
@@ -89,47 +92,171 @@ then
 
 
     w  # Write changes and exit
-EOF
-else
+  EOF
+  else
     echo "something went wrong when creating partitions. Please cancel the script with ctrl + c"
     sleep 3600
+  fi
+elif [ "$encryptFlag" = 'yes' ]
+then
+  if [ "$BiosOrUefi" = '64' ]
+  then
+    if [[ "$DISK" == /dev/nvme* ]]
+    then
+      cryptsetup luksFormat "$DISK"p1
+      echo "Please introduce your password to open drive"
+      crypsetup open "$DISK"p2 rootDrive
+    elif [[ "$DISK" == /dev/sd* ]]; then
+      cryptsetup luksFormat "$DISK"2
+      echo "Please introduce your password to open drive"
+      crypsetup open "$DISK"2 rootDrive
+    fi
+    # Create a GPT partition table (for UEFI systems)
+    sudo fdisk "$DISK" << EOF
+    g  # Create a new empty partition table (GPT)
+    n  # New partition for EFI System (512MB)
+    p
+    1
+
+    +512M
+    n  # New partition for root (/) (remaining space)
+    p
+    2
+
+
+    w  # Write changes and exit
+    EOF
+  elif [ "$BiosOrUefi" = '32' ]
+  then
+    if [[ "$DISK" == /dev/nvme* ]]
+    then
+      cryptsetup luksFormat "$DISK"p2
+      echo "Please introduce your password to open drive"
+      crypsetup open "$DISK"p2 rootDrive
+    elif [[ "$DISK" == /dev/sd* ]]; then
+      cryptsetup luksFormat "$DISK"2
+      echo "Please introduce your password to open drive"
+      crypsetup open "$DISK"2 rootDrive
+    fi
+
+    sudo fdisk "$DISK" << EOF
+    g  # Create a new empty partition table (GPT)
+    n  # New partition for EFI System (512MB)
+    p
+    1
+
+    +512M
+    n  # New partition for root (/) (remaining space)
+    p
+    2
+
+
+    w  # Write changes and exit
+  EOF
+  elif [ "$BiosOrUefi" = '0' ]
+  then
+    if [[ "$DISK" == /dev/nvme* ]]
+    then
+        cryptsetup luksFormat "$DISK"p2
+        echo "Please introduce your password to open drive"
+        crypsetup open "$DISK"p2 rootDrive
+    elif [[ "$DISK" == /dev/sd* ]]; then
+        cryptsetup luksFormat "$DISK"2
+        echo "Please introduce your password to open drive"
+        crypsetup open "$DISK"2 rootDrive
+    fi
+
+    sudo fdisk  "$DISK" << EOF
+    o # Create new empy partition table (MBR)
+    n  # New partition for EFI System (512MB)
+    p
+    1
+
+    +512M
+    n  # New partition for root (/) (remaining space)
+    p
+    2
+
+
+    w  # Write changes and exit
+  EOF
+  else
+      echo "something went wrong when creating partitions. Please cancel the script with ctrl + c"
+      sleep 3600
+  fi
 fi
 
 
 ###Checks if drive is sata or mvme to create file systems and mounting.
-if [[ "$DISK" == /dev/nvme* ]]; then
-    echo "Formatting partitions..."
-    sudo mkfs.fat -F 32 "${DISK}"p1  # EFI System Partition (ESP)
-    sudo mkfs.ext4 "${DISK}"p2       # Root partition
+if [ "$encryptFlag" = 'no' ]
+then
+  if [[ "$DISK" == /dev/nvme* ]]; then
+      echo "Formatting partitions..."
+      sudo mkfs.fat -F 32 "${DISK}"p1  # EFI System Partition (ESP)
+      sudo mkfs.ext4 "${DISK}"p2       # Root partition
 
-    # Mount the partitions for installation
-    echo "Mounting partitions..."
-    mount "${DISK}"p2 /mnt
-    mkdir -p /mnt/boot
-    mount "${DISK}"p1 /mnt/boot
+      # Mount the partitions for installation
+      echo "Mounting partitions..."
+      mount "${DISK}"p2 /mnt
+      mkdir -p /mnt/boot
+      mount "${DISK}"p1 /mnt/boot
 
-elif [[ "$DISK" == /dev/sd* ]]; then
-    echo "Formatting partitions..."
-    sudo mkfs.fat -F 32 "${DISK}"1  # EFI System Partition (ESP)
-    sudo mkfs.ext4 "${DISK}"2       # Root partition
+  elif [[ "$DISK" == /dev/sd* ]]; then
+      echo "Formatting partitions..."
+      sudo mkfs.fat -F 32 "${DISK}"1  # EFI System Partition (ESP)
+      sudo mkfs.ext4 "${DISK}"2       # Root partition
 
-    # Mount the partitions for installation
-    echo "Mounting partitions..."
-    mount "${DISK}"2 /mnt
-    mkdir -p /mnt/boot
-    mount "${DISK}"1 /mnt/boot
-else
-    echo "Formatting partitions for virtual drive..."
-    sudo mkfs.fat -F 32 "${DISK}"1  # EFI System Partition (ESP)
-    sudo mkfs.ext4 "${DISK}"2       # Root partition
+      # Mount the partitions for installation
+      echo "Mounting partitions..."
+      mount "${DISK}"2 /mnt
+      mkdir -p /mnt/boot
+      mount "${DISK}"1 /mnt/boot
+  else
+      echo "Formatting partitions for virtual drive..."
+      sudo mkfs.fat -F 32 "${DISK}"1  # EFI System Partition (ESP)
+      sudo mkfs.ext4 "${DISK}"2       # Root partition
 
-    # Mount the partitions for installation
-    echo "Mounting partitions..."
-    mount "${DISK}"2 /mnt
-    mkdir -p /mnt/boot
-    mount "${DISK}"1 /mnt/boot
+      # Mount the partitions for installation
+      echo "Mounting partitions..."
+      mount "${DISK}"2 /mnt
+      mkdir -p /mnt/boot
+      mount "${DISK}"1 /mnt/boot
+  fi
+elif [ "$encryptFlag" = 'yes' ]
+then
+    if [[ "$DISK" == /dev/nvme* ]]; then
+        echo "Formatting partitions..."
+        sudo mkfs.fat -F 32 "${DISK}"p1  # EFI System Partition (ESP)
+        sudo mkfs.ext4 /dev/mapper/rootDrive       # Root partition
+
+        # Mount the partitions for installation
+        echo "Mounting partitions..."
+        mount /dev/mapper/rootDrive /mnt
+        mkdir -p /mnt/boot
+        mount "${DISK}"p1 /mnt/boot
+
+    elif [[ "$DISK" == /dev/sd* ]]; then
+        echo "Formatting partitions..."
+        sudo mkfs.fat -F 32 "${DISK}"1  # EFI System Partition (ESP)
+        sudo mkfs.ext4 /dev/mapper/rootDrive       # Root partition
+
+        # Mount the partitions for installation
+        echo "Mounting partitions..."
+        mount /dev/mapper/rootDrive /mnt
+        mkdir -p /mnt/boot
+        mount "${DISK}"1 /mnt/boot
+    else
+        echo "Formatting partitions for virtual drive..."
+        sudo mkfs.fat -F 32 "${DISK}"1  # EFI System Partition (ESP)
+        sudo mkfs.ext4 /dev/mapper/rootDrive       # Root partition
+
+        # Mount the partitions for installation
+        echo "Mounting partitions..."
+        mount /dev/mapper/rootDrive /mnt
+        mkdir -p /mnt/boot
+        mount "${DISK}"1 /mnt/boot
+    fi
 fi
-
 echo "Done. Proceeding with Arch Linux installation."
 
 ### Checks if cpu is intel, amd o virtualize
@@ -149,8 +276,10 @@ if [ "$cpu_vendor" = "Intel" ]
 then
     pacstrap -K /mnt base linux linux-firmware intel-ucode grub man-db man-pages texinfo vi vim eza networkmanager ntp bat alacritty kitty sudo fastfetch ufw
 elif [ "$cpu_vendor" = "AMD" ]
+then
     pacstrap -K /mnt base linux linux-firmware amd-ucode grub man-db man-pages texinfo vi vim eza networkmanager ntp bat alacritty kitty sudo fastfetch ufw
 elif [ "$cpu_vendor" = "virtualMachine" ]
+then
     pacstrap -K /mnt base linux linux-firmware grub man-db man-pages texinfo vi vim eza networkmanager ntp bat alacritty kitty sudo fastfetch ufw
 fi
 
