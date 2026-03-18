8c78b1741780972649ffb5a4ba1e7e31f9494dd5
CarlosR759 <80783790+CarlosR759@users.noreply.github.com>
2026-03-18 19:43:32 -0300

    Fixed name for mounted partition as how the script makes

--- archInstaller.sh
+++ archInstaller.sh
@@ -373,17 +373,16 @@ echo "$partitionLuskID"
 
 #sed -i "/^GRUB_CMDLINE_LINUX_DEFAULT=/ s/\(\".*\"\)/\1 cryptdevices=UUID=$partitionLuskID:cryptlvm root=UUID=$partitionHardwareID/" /mnt/etc/default/grub
 #sed -i "/^GRUB_CMDLINE_LINUX_DEFAULT=/ s/\(\".*\"\)/\1 cryptdevices=UUID=$partitionLuskID:cryptlvm root=UUID=$partitionHardwareID/" /mnt/etc/default/grub
-sed -i "/^GRUB_CMDLINE_LINUX_DEFAULT=/ s/\"$/ cryptdevice=UUID=$partitionHardwareID:cryptlvm root=UUID=$partitionLuskID\"/" /mnt/etc/default/grub
+sed -i "/^GRUB_CMDLINE_LINUX_DEFAULT=/ s/\"$/ cryptdevice=UUID=$partitionHardwareID:rootDrive root=UUID=$partitionLuskID\"/" /mnt/etc/default/grub
 #sed -i "/^HOOKS=/ s/\(([^)]*)\)/(\1 encrypt lvm2)/" /mnt/etc/mkinitcpio.conf Delete this in the future.
 sed -i "/^HOOKS=/ s/\([^)]*\)/\1 encrypt lvm2/" /mnt/etc/mkinitcpio.conf
 
-fstabgen -U /mnt >> /mnt/etc/fstab
 cp /root/ArchLinuxInstaller/endingSetup.sh /mnt/endingSetup.sh
 
 echo "Checking variables before chrooting: "
 echo "DISK: $DISK"
 echo "BiosOrUefi: $BiosOrUefi"
-sleep 10
+sleep 2
 arch-chroot /mnt /endingSetup.sh "$DISK" "$BiosOrUefi"
 
 umount -R /mnt
