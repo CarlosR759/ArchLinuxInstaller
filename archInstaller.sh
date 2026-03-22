25c1b9152f3d957a40ae01a6106d9a1cb131520c
CarlosR759 <80783790+CarlosR759@users.noreply.github.com>
2026-03-22 16:33:24 -0300

    Fixied initramfs for encrypted devices

--- archInstaller.sh
+++ archInstaller.sh
@@ -377,6 +377,11 @@ echo "$partitionLuskID"
 sed -i "/^GRUB_CMDLINE_LINUX_DEFAULT=/ s/\"$/ cryptdevice=UUID=$partitionHardwareID:rootDrive\"/" /mnt/etc/default/grub
 #sed -i "/^HOOKS=/ s/\(([^)]*)\)/(\1 encrypt lvm2)/" /mnt/etc/mkinitcpio.conf Delete this in the future.
 #sed -i "/^HOOKS=/ s/\([^)]*\)/\1 encrypt lvm2/" /mnt/etc/mkinitcpio.conf
+
+#Making a busybox approach instead of systemd
+sed -i '/^HOOKS=/ s/\bsystemd\b//' /mnt/etc/mkinitcpio.conf
+sed -i 's/\(base\)/\1 udev/' /mnt/etc/mkinitcpio.conf
+sed -i 's/\(sd-vconsole\)/\1 consolefont/' /mnt/etc/mkinitcpio.conf
 sed -i 's/\(block\)/\1 encrypt lvm2/' /mnt/etc/mkinitcpio.conf
 
 
