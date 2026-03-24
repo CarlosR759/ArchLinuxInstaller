c794e535e547e5487f5920181c11181873e4a5a0
CarlosR759 <80783790+CarlosR759@users.noreply.github.com>
2026-03-24 10:07:46 -0300

    Added wallpaper, dotfiles and bashrc for Hyprland setup

--- chrootPart.sh
+++ chrootPart.sh
@@ -175,12 +175,20 @@ then
     mkdir -p /home/"$User"/.config/hypr/
     cd /home/"$User"/.config/hypr/
     git clone https://github.com/CarlosR759/HyprConfs .
+    cd /home/"$User"
+    git clone https://github.com/CarlosR759/wallpapers
+    git clone https://github.com/CarlosR759/mydotfiles .
+    wget -O ~/.bashrc https://raw.githubusercontent.com/CarlosR759/bashrc/main/bashrc
 elif [[ "$hyprAnswer" = 'yes' && "$User" == '0' ]]
 then
     pacman -S git hyprland hyprpaper hyprpicker hyprlock xdg-desktop-portal-hyprland hyprpolkitagent hyprsunset rofi lf calcurse fzf nerd-fonts gnu-free-fonts ttf-font-awesome noto-fonts-emoji ttf-iosevka-nerd --noconfirm
     mkdir -p /home/root/.config/hypr/
     cd /home/root/.config/hypr/
     git clone https://github.com/CarlosR759/HyprConfs .
+    cd  /home/root
+    git clone https://github.com/CarlosR759/wallpapers
+    git clone https://github.com/CarlosR759/mydotfiles .
+    wget -O ~/.bashrc https://raw.githubusercontent.com/CarlosR759/bashrc/main/bashrc
 fi
 
 systemctl enable ufw
